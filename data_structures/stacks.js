fca0b124215d1b8c9bfd8b77cdf5792f77698ea6
Kevin Heleodoro <kevin.heleodoro@gmail.com>
2022-01-07 18:15:35 -0600

    ES6 stacks class created

---
+++ data_structures/stacks.js
@@ -0,0 +1,17 @@
+export default class Stack {
+    constructor(){
+        this.items = [];
+    }
+
+    push(string){
+        this.items.push(string);
+    }
+
+    pop(){
+        return this.items.pop();
+    }
+
+    size(){
+        return this.items.length
+    }
+}
--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 import Queue from "./data_structures/queues.js";
+import Stack from "./data_structures/stacks.js";
 
 const cars = new Queue();
 
 cars.enqueue("Jetta")
 cars.enqueue("Civic")
 cars.enqueue("Cherokee")
+cars.dequeue();
+cars.size();
+cars.isEmpty();
 
-console.log(cars)
+// console.log(cars)
+
+const plates = new Stack();
+
+plates.push("Fine China")
+plates.push("Dixie Double Thick")
+plates.push("Piece of Paper Towel")
+plates.size();
+plates.pop();
+
+console.log(plates)
